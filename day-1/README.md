c6d663d7519d8f6d69702f2ab1bc1a1c380e70b2
Alain Baxter <alain.baxter@digitalturbine.com>
2023-12-20 16:58:19 -0500

    Add part 2 of day 1 success

--- day-1/README.md
+++ day-1/README.md
@@ -2,12 +2,25 @@
 
 link: https://adventofcode.com/2023/day/1
 
-Running code to solve:
+Running code to solve part 1 example input:
 ```
-root@wsl:day-1# go run main.go input.txt
+root@wsl:day-1# go run main.go part1-sample.txt
 1abc2 parsed digits are: 12
 pqr3stu8vwx parsed digits are: 38
 a1b2c3d4e5f parsed digits are: 15
 treb7uchet parsed digits are: 77
 Sum of digits is: 142
+```
+
+Running code to solve part 2 example input:
+```
+root@wsl:day-1# go run main.go part2-sample.txt
+two1nine parsed digits are: 29
+eightwothree parsed digits are: 83
+abcone2threexyz parsed digits are: 13
+xtwone3four parsed digits are: 24
+4nineeightseven2 parsed digits are: 42
+zoneight234 parsed digits are: 14
+7pqrstsixteen parsed digits are: 76
+Sum of digits is: 281
 ```
--- day-1/main.go
+++ day-1/main.go
@@ -9,6 +9,18 @@ import (
 	"unicode"
 )
 
+var stringDigits = map[string]int{
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
 func main() {
     var filepath string
 
@@ -29,7 +41,7 @@ func main() {
     sum := 0
     for scanner.Scan() {
         text := scanner.Text()
-        num, _ := getFirstAndLastDigits(text)
+        num := getFirstAndLastDigits(text)
         fmt.Printf("%s parsed digits are: %d\n", text, num)
         sum += num
     }
@@ -41,20 +53,42 @@ func main() {
     }
 }
 
-func getFirstAndLastDigits(s string) (int, error) {
-    var digits []byte
+func getFirstAndLastDigits(s string) int {
+    var digits []int
+    var buffer = ""
     for i, r := range s {
         if unicode.IsDigit(r) {
-            digits = append(digits, s[i])
+            num, _ := strconv.Atoi(string(s[i]))
+            digits = append(digits, num)
+            buffer = ""
+        } else {
+            buffer = buffer + string(s[i])
+            check := checkForSpeltDigit(buffer)
+            if check != 0 {
+                digits = append(digits, check)
+                // keep the last buffer character in case of overlap? (e.g. twone can be two then one)
+                buffer = buffer[(len(buffer)-1):]
+            }
         }
     }
 
     if len(digits) == 0 {
-        return 0, nil
-    } else if len(digits) == 1 {
-        digits = append(digits, digits[0])
-        return strconv.Atoi(string(digits))
+        return 0
     } else {
-        return strconv.Atoi(string([]byte{digits[0], digits[len(digits) - 1]}))
+        r, _ := strconv.Atoi(fmt.Sprintf("%d%d", digits[0], digits[len(digits) - 1]))
+        return r
+    }
+}
+
+func checkForSpeltDigit(s string) int {
+    if len(s) < 3 {
+        return 0
+    }
+
+    val, ok := stringDigits[s]
+    if ok {
+        return val
+    } else {
+        return checkForSpeltDigit(s[1:])
     }
 }
